32488d2c85c0a022a150cf62bde90f16fa99bd54
cbasconc1 <cbasconc3132@gmail.com>
2025-03-26 11:38:14 -0500

    ...

--- restCounter/src/main/java/com/restCounter/restCounter/RestCounterApplication.java
+++ restCounter/src/main/java/com/restCounter/restCounter/RestCounterApplication.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
 @ComponentScan({"com.restCounter.restCounter", "infrastructure.Services"})
